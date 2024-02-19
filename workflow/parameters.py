73585e026792db6629c5cf9f99b0cd14b72dbdcc
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2024-02-19 13:16:17 +0100

    remove dead code

--- workflow/parameters.py
+++ workflow/parameters.py
@@ -5,25 +5,6 @@ import pickle
 import hashlib
 
 
-def get_data_path(base_dir):
-    import os
-
-    def inner(wildcards):
-        print(wildcards)
-        hdf5_datasets = {
-            "fashion-mnist-784-euclidean",
-            "glove-100-angular",
-            "nytimes-256-angular",
-        }
-        if wildcards.dataset in hdf5_datasets:
-            ext = "hdf5"
-        else:
-            ext = "bin"
-        return os.path.join(base_dir, f"{wildcards.dataset}.{ext}")
-
-    return inner
-
-
 class WorkloadPatterns:
     def __init__(self, configs, workloads_dict):
         self._configs = configs
@@ -103,14 +84,7 @@ def _file_based_workloads():
     return configs, workloads_dict
 
 
-def workloads():
-    """Builds the configuration of all workloads that we use in our experiments.
-
-    Returns a pair, where the first element is the paramspace to be used
-    in the workflow, and the second is a dictionary where the keys are
-    workload identifiers (used in the paths as part of the wildcard)
-    and the values are dictionaries holding the workload configuration.
-    """
+def _annealing_workloads():
     import sys
     import os
 
@@ -122,8 +96,6 @@ def workloads():
 
     configs = []
     workloads_dict = dict()
-
-    # TODO: add builtin queries, including the ones that are not generated
 
     datasets = [
         "fashion_mnist-euclidean-784-60K",
@@ -194,10 +166,34 @@ def workloads():
                     }
                 )
 
-    # Gaussian noise workloads
+    return configs, workloads_dict
+
+
+def _gaussian_noise_workloads():
+    import sys
+    import os
+
+    # Make the read_data module visible by adding its directory to
+    # the search path.
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+    import read_data as rd
+
+    configs = []
+    workloads_dict = dict()
+
     workload_type = "synthetic-gaussian-noise"
+
+    datasets = [
+        "fashion_mnist-euclidean-784-60K",
+        # "glove-angular-32-1183514",
+        # "glove-angular-104-1183514",
+        # "nytimes-angular-256-289761",
+    ]
     scales = [0.1, 1.0, 10.0]
     num_queries = [300]
+    k_values = [10]
+
     for dataset, k, nq in product(datasets, k_values, num_queries):
         for scale in scales:
             conf = {
@@ -219,88 +215,31 @@ def workloads():
                 }
             )
 
+    return configs, workloads_dict
+
+
+def workloads():
+    """Builds the configuration of all workloads that we use in our experiments.
+
+    Returns the WorkloadPatterns instance containing all the workloads we want to run.
+    """
+
+    configs = []
+    workloads_dict = dict()
+
+    annealing_configs = _annealing_workloads()
+    configs.extend(annealing_configs[0])
+    workloads_dict.update(annealing_configs[1])
+
+    noise_configs = _gaussian_noise_workloads()
+    configs.extend(noise_configs[0])
+    workloads_dict.update(noise_configs[1])
+
     file_configs = _file_based_workloads()
     configs.extend(file_configs[0])
     workloads_dict.update(file_configs[1])
 
     return WorkloadPatterns(configs, workloads_dict)
-
-
-def setup_param_space():
-    datasets = [
-        "fashion-mnist-784-euclidean",
-        "glove-100-angular",
-        "sald-128-1000000",
-        "nytimes-256-angular",
-    ]
-    queries = {
-        "sald": ["sald", "sald-noise-10", "sald-noise-30", "sald-noise-50"],
-        "glove-100-bin": [
-            "glove-noise-0",
-            "glove-noise-10",
-            "glove-noise-30",
-            "glove-noise-50",
-        ],
-    }
-    target_difficulty = {
-        "faiss_ivf": {
-            "fashion-mnist-784-euclidean": [
-                (t - 0.01, t + 0.01) for t in [0.05, 0.1, 0.3]
-            ],
-            "glove-100-angular": [(t - 0.01, t + 0.01) for t in [0.05, 0.1, 0.3]],
-            "sald-128-1000000": [(t - 0.01, t + 0.01) for t in [0.05, 0.1, 0.3]],
-            "nytimes-256-angular": [(t - 0.01, t + 0.01) for t in [0.05, 0.1, 0.3]],
-        },
-        "rc": {
-            "fashion-mnist-784-euclidean": [],
-            "glove-100-angular": [],
-            "sald-128-1000000": [],
-            "nytimes-256-angular": [],
-        },
-    }
-    num_queries = {
-        "fashion-mnist-784-euclidean": [5],
-        "glove-100-angular": [5],
-        "sald-128-1000000": [5],
-        "nytimes-256-angular": [5],
-    }
-    scale = {
-        "fashion-mnist-784-euclidean": [10],
-        "glove-100-angular": [0.1],
-        "sald-128-1000000": [10],
-        "nytimes-256-angular": [0.1],
-    }
-    initial_temperature = {
-        "fashion-mnist-784-euclidean": [10],
-        "glove-100-angular": [1],
-        "sald-128-1000000": [10],
-        "nytimes-256-angular": [1],
-    }
-    ks = [10]
-
-    configs = []
-
-    for dataset in datasets:
-        for nq in num_queries[dataset]:
-            for k in ks:
-                for tf in target_difficulty.keys():
-                    if dataset in target_difficulty[tf]:
-                        for lower, upper in target_difficulty[tf][dataset]:
-                            for s in scale[dataset]:
-                                for temp in initial_temperature[dataset]:
-                                    configs.append(
-                                        {
-                                            "dataset": dataset,
-                                            "k": k,
-                                            "num_queries": nq,
-                                            "difficulty": tf,
-                                            "target_lower": lower,
-                                            "target_upper": upper,
-                                            "scale": s,
-                                            "initial_temperature": temp,
-                                        }
-                                    )
-    return Paramspace(pd.DataFrame(configs))
 
 
 if __name__ == "__main__":
