e23ba91c9057cc4765262dd7ab844c7c26c8af3b
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2024-05-23 17:37:32 +0200

    update with results from Nefeli

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [
+            R
+            python311
+            ruff
+            snakemake
+          ] ++
+          (with pkgs.python311Packages; [
+            pip
+            venvShellHook
+            h5py
+            numpy
+            jax
+            optax
+            seaborn
+            matplotlib
+            scikit-learn
+            joblib
+            tqdm
+            icecream
+            statsmodels
+            jinja2
+            jupyter-console
+            radian
+          ]) ++
+          (with pkgs.rPackages; [
+            tidyverse
+            kableExtra
+          ])
+          ;
+        };
+      });
+    };
+}
