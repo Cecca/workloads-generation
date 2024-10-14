d475fc7b46c635551e723f8e7daacb49c945d6a6
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2024-10-14 16:44:53 +0200

    add plot on empirical results

---
+++ workflow/plots/empirical_workload_results.py
@@ -0,0 +1,32 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica"
+})
+
+df = pd.read_csv(snakemake.input[0])
+df = df[df["distcomp"] <= 0.2]
+df = df[df["index_name"] != "messi_apprx"]
+df["dataset"] = df["dataset"].str.replace(".*/", "", regex=True)
+df["dataset"] = df["dataset"].str.replace("-.*", "", regex=True)
+df = df[df["dataset"] != "fashion_mnist"]
+df = df[df["dataset"] != "rw"]
+ndatasets = len( df["dataset"].unique() )
+n_indices = len( df["index_name"].unique() )
+
+plt.figure(figsize=(6,3))
+sns.stripplot(
+    data = df,
+    x = "distcomp",
+    y = "dataset"
+)
+plt.xlabel(r"MESSI empirical hardness ($\mathcal{H}_{MESSI}$)")
+plt.axvline(0.1, c="lightgray", linestyle="dotted")
+plt.axvline(0.2, c="lightgray", linestyle="dotted")
+plt.xlim((0,1))
+
+plt.tight_layout(pad=0)
+plt.savefig(snakemake.output[0], dpi=300)
